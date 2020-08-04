f35d2f22ee68467c39f78c8d8ac785e5c1233508
Swapnil Jadon <swapniljadon@gmail.com>
2020-08-05 01:20:59 +0530

    Create Roman to Integer.java

---
+++ Roman to Integer.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int romanToInt(String s) {
+        int num=0;
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='I'){
+                if((i<(n-1))&&(s.charAt(i+1)=='V')){
+                    num+=4;
+                    i++;
+                }
+                else if((i<(n-1))&&(s.charAt(i+1)=='X')){
+                    num+=9;
+                    i++;
+                }
+                else
+                    num+=1;
+            }
+            else if(s.charAt(i)=='V')
+                num+=5;
+            else if(s.charAt(i)=='X'){
+                if((i<(n-1))&&(s.charAt(i+1)=='L')){
+                    num+=40;
+                    i++;
+                }
+                else if((i<(n-1))&&(s.charAt(i+1)=='C')){
+                    num+=90;
+                    i++;
+                }
+                else
+                    num+=10;
+            }
+            else if(s.charAt(i)=='L')
+                num+=50;
+            else if(s.charAt(i)=='C'){
+                if((i<(n-1))&&(s.charAt(i+1)=='D')){
+                    num+=400;
+                    i++;
+                }
+                else if((i<(n-1))&&(s.charAt(i+1)=='M')){
+                    num+=900;
+                    i++;
+                }
+                else
+                    num+=100;
+            }
+            else if(s.charAt(i)=='D')
+                num+=500;
+            else if(s.charAt(i)=='M')
+                num+=1000;
+        }
+        return num;
+    }
+}
