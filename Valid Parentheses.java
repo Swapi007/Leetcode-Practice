f90784164640d1f97d5aa73ab99ae8401ae3a98a
Swapnil Jadon <swapniljadon@gmail.com>
2020-08-06 01:32:52 +0530

    Create Valid Parentheses.java

---
+++ Valid Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==0)
+            return true;
+        else if((s.charAt(0)==')')||(s.charAt(0)==']')||(s.charAt(0)=='}'))
+            return false;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if((c=='(')||(c=='[')||(c=='{'))
+                st.push(c);
+            else if((c==')')&&(!st.isEmpty())){
+                if(st.peek()=='(')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if((c==']')&&(!st.isEmpty())){
+                if(st.peek()=='[')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if((c=='}')&&(!st.isEmpty())){
+                if(st.peek()=='{')
+                    st.pop();
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        return st.isEmpty();
+    }
+}
